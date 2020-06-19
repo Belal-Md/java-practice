3bd9bb0972bebc0ea8423e47d9efc4e78082ae8d
Belal-Md <56088749+Belal-Md@users.noreply.github.com>
2020-06-19 22:34:57 +0600

    Example: object reference (added)
    Another example of the object reference.

---
+++ Day_2/ObjectReferenceExample.java
@@ -0,0 +1,17 @@
+public class ObjectReferenceExample {
+
+	public static void main(String... doYourBest) {
+    	    Simpson simpson = new Simpson();
+    	    transformIntoHomer(simpson);
+    	    System.out.println(simpson.name);
+	}
+
+	static void transformIntoHomer(Simpson simpson) {
+    	    simpson.name = "Homer";
+	}
+
+}
+
+class Simpson {
+	String name;
+}
